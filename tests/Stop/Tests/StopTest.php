17d038ece0bd3503f31d050f1547b0865a211970
ivo <ivo.bathke@gmail.com>
2013-11-15 18:31:54 +0100

    added Context aware test

---
+++ tests/Stop/Tests/StopTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Stop\Tests;
+
+use Stop\Dumper\AbstractDumper;
+use Stop\Stop;
+
+require_once __DIR__ . '/../../../src/Stop/Dumper/DumperInterface.php';
+require_once __DIR__ . '/../../../src/Stop/Dumper/AbstractDumper.php';
+require_once __DIR__ . '/../../../src/Stop/Dumper/Text.php';
+require_once __DIR__ . '/../../../src/Stop/Model/Dump.php';
+require_once __DIR__ . '/../../../src/Stop/Stop.php';
+
+class StopTest extends \PHPUnit_Framework_TestCase {
+
+    public function testContextAware() {
+        #if console it should reder Text mode
+        $output = Stop::it('String', AbstractDumper::VAR_DUMP, false, false, true);
+        $this->assertContains('string(6) "String"', $output, 'var_dump the variable');
+        $this->assertContains('------------------------', $output, 'Is Text Mode');
+    }
+
+}
